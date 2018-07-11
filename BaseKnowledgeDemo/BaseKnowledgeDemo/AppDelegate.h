0515fd1394de9d03d634564979b9984acfbfa840
杜红星 <dhx@duhongxingdeMacBook-Pro.local>
2018-07-11 15:21:11 +0800

    first commit

---
+++ BaseKnowledgeDemo/BaseKnowledgeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseKnowledgeDemo
+//
+//  Created by 杜红星 on 2018/7/11.
+//  Copyright © 2018年 MrDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseKnowledgeDemo/BaseKnowledgeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseKnowledgeDemo
+//
+//  Created by 杜红星 on 2018/7/11.
+//  Copyright © 2018年 MrDu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
